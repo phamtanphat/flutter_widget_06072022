18a8742fe30b245c7d72facb5f7f5bf70b070a4e
phamtanphat <phatdroid94@gmail.com>
2022-07-27 18:57:59 +0700

    font

--- lib/lifecycle_widget.dart
+++ lib/lifecycle_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 class LifecycleWidget extends StatefulWidget {
 
@@ -10,6 +12,19 @@ class _LifecycleWidgetState extends State<LifecycleWidget> {
   @override
   void initState() {
     super.initState();
+    print("initState");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
+
+  @override
+  void didUpdateWidget(covariant LifecycleWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
   }
 
   @override
@@ -20,6 +35,25 @@ class _LifecycleWidgetState extends State<LifecycleWidget> {
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
+        child: Column(
+          children: [
+            Flexible(
+                flex: 2,
+                child: Center(
+                  child: Text(
+                      "Máy tính bỏ túi",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 30,
+                          fontFamily: "Roboto",
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold
+                      )
+                  ),
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
